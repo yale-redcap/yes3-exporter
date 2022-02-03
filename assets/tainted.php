5cdacd11c08639023dcdaf19fd55830d43c1b6fb
criwebtools <criwebtools@gmail.com>
2022-02-02 22:50:04 -0500

    added php errors to tainted.php

--- assets/tainted.php
+++ assets/tainted.php
@@ -57,6 +57,11 @@ class InputFilter {
 
         return null;
     }
+
+    prodected function foo( $bar )
+    {
+        return strfatime("%F %T");
+    }
 }
 
 echo (new InputFilter("hello"))->getArg("get", "string");
